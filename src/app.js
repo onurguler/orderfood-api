1ef63f51bc962532601b2423185bf25d293c8e5e
Onur Osman Güler <guleronurosman@gmail.com>
2020-12-02 22:56:38 +0300

    Setup error handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const httpStatus = require('http-status');
 const config = require('./config');
+const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
 
 const app = express();
 
@@ -8,5 +11,16 @@ app.set('port', config.port);
 app.get('/', (req, res) => {
   res.send('API is running.');
 });
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 module.exports = app;
