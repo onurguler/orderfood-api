767f38f8f12f35ee8e75cac0e11e21a62e0db8ef
Onur Osman Güler <guleronurosman@gmail.com>
2020-12-02 16:26:54 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send('API is running.');
+});
+
+module.exports = app;
