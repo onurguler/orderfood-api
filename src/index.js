a817406602c65668333e99318d70322c46280a50
Onur Osman Güler <guleronurosman@gmail.com>
2020-12-02 16:57:21 +0300

    Setup mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+const config = require('./config');
 const logger = require('./config/logger');
 
-app.listen(app.get('port'), () => {
-  logger.info(`Server is running on http://localhost:${app.get('port')}`);
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('Connected to MongoDB.');
+  app.listen(app.get('port'), () => {
+    logger.info(`Server is running on http://localhost:${app.get('port')}`);
+  });
 });
